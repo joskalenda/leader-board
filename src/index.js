24528d589fec328011cee9a9d105e5f1598b1676
joskalenda <joskalenda3@gmail.com>
2022-01-27 15:18:56 +0200

    Add function to display players

--- src/index.js
+++ src/index.js
@@ -1,41 +1,36 @@
 import './style.css';
+// import {king, crown, win2, win3} from '../asset/king.jpeg';
+import { ShownNewUser, CheckIfNumber,  } from '../validation';
+import { getUserScore} from '../set__api';
+
+const number = document.querySelector('#number');
+const userName = document.querySelector('#user--name');
 
 const PlayerSection = document.querySelector('.player--section');
+const playNow = document.querySelector('#play--now');
+const submitScore = document.querySelector('#submit--score');
+const reloadPage = document.querySelector('.title--king i');
 
-const PlayerScore = [
-  {
-    index: 1,
-    name: 'Topaz',
-    score: 100,
-  },
-  {
-    index: 2,
-    name: 'Karis',
-    score: 100,
-  },
-  {
-    index: 3,
-    name: 'Ben',
-    score: 100,
-  },
-  {
-    index: 4,
-    name: 'Carter',
-    score: 100,
-  },
-];
 
-const DisplayPlayer = () => {
-  PlayerScore.forEach((e) => {
+const DisplayPlayer = (Player) => {
+  Player.forEach((e) => {
     const player = document.createElement('div');
     player.classList.add('player--div');
-    player.id = e.index;
     player.innerHTML = `
-    <p id="player--index">${e.index}</p>
-    <h3 id="player--name">${e.name}</h3>
+    <p id="player--index">
+    <img src="${king}"></p>
+    <h3 id="player--name">${e.user}</h3>
     <p id="player--score">${e.score}</p>`;
     PlayerSection.appendChild(player);
   });
 };
 
-DisplayPlayer();
+
+const display = async () => {
+  const getUser = await getUserScore();
+  getUser.result.sort((a, b) => b.score - a.score);
+  DisplayPlayer(getUser.result);
+
+}
+
+
