3ade169c3a0a1bacaf6e611321bf4ce4129ad700
joskalenda <joskalenda3@gmail.com>
2022-01-28 02:20:50 +0200

    Add 1st 3 player scores

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
-// import {king, crown, win2, win3} from '../asset/king.jpeg';
 import { ShownNewUser, CheckIfNumber,  } from '../validation';
-import { getUserScore} from '../set__api';
+import win from '../asset/win2.jpeg';
+import crown from '../asset/crown.png';
+import { FetchNewScore} from '../set__api';
+
 
 const number = document.querySelector('#number');
 const userName = document.querySelector('#user--name');
@@ -10,39 +12,55 @@ const PlayerSection = document.querySelector('.player--section');
 const playNow = document.querySelector('#play--now');
 const submitScore = document.querySelector('#submit--score');
 const reloadPage = document.querySelector('.title--king i');
+const king = document.querySelector('#fs-w');
+const king_Score = document.querySelector('#k--score');
+const sec_Score = document.querySelector('#sec--score');
+const th_Score = document.querySelector('#th--score');
+king.innerHTML = `<img src="${crown}">`;
 
 
+
+//RENDER PLAYER FUNCTION
 const DisplayPlayer = (Player) => {
+  PlayerSection.innerHTML = '';
   Player.forEach((e) => {
     const player = document.createElement('div');
     player.classList.add('player--div');
     player.innerHTML = `
     <p id="player--index">
-    <img src="${king}"></p>
-    <h3 id="player--name">${e.user}</h3>
+    <img src="${win}"></p>
+    <h3 id="player--name">@${e.user}</h3>
     <p id="player--score">${e.score}</p>`;
     PlayerSection.appendChild(player);
   });
 };
 
 
-const display = async () => {
-  const getUser = await getUserScore();
-  getUser.result.sort((a, b) => b.score - a.score);
-  DisplayPlayer(getUser.result);
+const LoadData = async () => {
+  const User = await FetchNewScore();
+  User.result.sort((a, b) => b.score - a.score);
+  // 1st 3 players scores
+  king_Score.innerHTML =`${User.result[0].user}: ${User.result[0].score} `;
+  sec_Score.innerHTML =`${User.result[1].user}: ${User.result[1].score} `;
+  th_Score.innerHTML =`${User.result[2].user}: ${User.result[2].score} `;
+
+  DisplayPlayer(User.result);
 
 }
-
+ 
 // add newplayer
 submitScore.addEventListener ('click', async (e) => {
   e.preventDefault();
   CheckIfNumber();
   number.value = '';
   userName.value = '';
-  display(); 
 });
+
 playNow.addEventListener ('click', () => {
   ShownNewUser();
 });
-reloadPage.addEventListener ('click', display);
+
+reloadPage.addEventListener ('click', LoadData);
+
+
 
