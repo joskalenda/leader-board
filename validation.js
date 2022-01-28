d975b90cbe8a4cf2d10d6b434ee3fe4cb9b05ae0
joskalenda <joskalenda3@gmail.com>
2022-01-28 18:34:21 +0200

    Update validation

--- validation.js
+++ validation.js
@@ -1,36 +1,35 @@
-
-import { addNewScore} from './set__api';
+import { addNewScore } from './set__api.js';
 
 const showForm = document.querySelector('#form-id');
 const number = document.querySelector('#number');
 const userName = document.querySelector('#user--name');
 
 export const CheckIfNumber = async () => {
-  if (isNaN(number.value)) {
-    document.getElementById("error").innerText = "Please enter Numeric score value";
-    setTimeout(function(){
-      document.getElementById("error").innerText = "";
-    }, 3000); 
-    return false;
-  } else if (number.value === '' || userName.value === '') {
-    document.getElementById("error").innerText = "field empty, please fill and resubmit ";
-    setTimeout(function() {
-      document.getElementById("error").innerText = "";
+
+  if ((isNaN(number.value))) {
+    document.getElementById('error').innerText = 'Please enter Numeric score value';
+    setTimeout(() => {
+      document.getElementById('error').innerText = '';
     }, 3000);
     return false;
-  } else if (number.value <= 10) {
-    document.getElementById("error").innerText = "Invalid, Score must be more than (10) ";
-    setTimeout(function() {
-      document.getElementById("error").innerText = "";
+  } if (number.value === '' || userName.value === '') {
+    document.getElementById('error').innerText = 'field empty, please fill and resubmit';
+    setTimeout(() => {
+      document.getElementById('error').innerText = '';
     }, 3000);
     return false;
-  }else {
-    // add newplayer if input valid 
-    await addNewScore();
-    return true;
-  };
+  } if (number.value <= 10) {
+    document.getElementById('error').innerText = 'Invalid, Score must be more than (10) ';
+    setTimeout(() => {
+      document.getElementById('error').innerText = '';
+    }, 3000);
+    return false;
+  }
+  // add newplayer if input valid
+  await addNewScore();
+  return true;
 };
 
 export const ShownNewUser = () => {
   showForm.style.display = 'flex';
-}
+};
